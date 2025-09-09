0d951b45ef57d535b292cd8de740f26333a2687c
Rizky Fadhilah <rizky.fadhilah@binus.ac.id>
2025-09-09 17:20:52 +0700

    home page done

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -3,8 +3,8 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { COLORS } from "../constants/colors";
 
 const SafeScreen = ({ children }) => {
-  const insets = useSafeAreaInsets();
 
+  const insets = useSafeAreaInsets();
   return (
     <View style={{ paddingTop: insets.top, flex: 1, backgroundColor: COLORS.background }}>
       {children}
